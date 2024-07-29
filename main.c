87452207f24fd80dcf089298a13f71f197df832f
mtelek <marcitelek@gmail.com>
2024-07-29 22:33:25 +0200

    fixed quote removal

--- main.c
+++ main.c
@@ -19,9 +19,14 @@ void remove_quotes(char *str, int start, int end)
 	i = 0;
 	while (str[i] != '\0' && str[i] != str[start])
 		i++;
-	while (str[i] != '\0' && str[i+1] != str[end])
+	while (str[i+1] != '\0' && str[i+1] != str[end])
 	{
 		str[i] = str[i+1];
+		i++;
+	}
+	while (str[i+2] != '\0')
+	{
+		str[i] = str[i+2];
 		i++;
 	}
 	str[i] = '\0';
@@ -36,17 +41,14 @@ void	delete_qoutes(t_lexer *lexer)
 
 	while (lexer != NULL)
 	{
-		temp_start = qoutes_checker(lexer->str, 34, -1); //not working for " if its not at the first place
+		temp_start = qoutes_checker(lexer->str, 34, -1);
 			start = qoutes_checker(lexer->str, 39, -1);
-		if (temp_start < start)
+		if ((temp_start != 0 && temp_start < start) || start == 0)
 			start = temp_start;
 		if (lexer->str[start])
 			end = qoutes_handler(lexer->str, start) - 1;
-		printf("Start:%d\n", start);
-		printf("End:%d\n", end);
-		if (end == 0 && start == 0)
-		 	return;
-		remove_quotes(lexer->str, start, end);
+		if (end != 0)
+			remove_quotes(lexer->str, start, end);
 		lexer = lexer->next;
 	}
 }
